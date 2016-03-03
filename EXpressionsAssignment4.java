6a8a67d110b04e1c40e1af59c41854bba8c5a177
himabindu-mahi <himabindu@sellerware.com>
2016-03-03 16:33:44 +0530

    Added files via upload

---
+++ EXpressionsAssignment4.java
@@ -0,0 +1,22 @@
+class ExpressionsAssignment4
+{
+	public static void main(String args[])
+	{
+	    int x=5, y=10;
+		System.out.println("Expressions X+Y*2 " +(x+y*2));
+		System.out.println("Expressions X-Y+2 " +(x-y+2));
+		System.out.println("Expressions (X+Y)*2   " + ((x+y)*2));
+		System.out.println("Expressions y%x " + (y%x));
+		
+	}
+}
+
+/* 
+Output:--
+
+Expressions X+Y*2 25
+Expressions X-Y+2 -3
+Expressions (X+Y)*2   30
+Expressions y%x 0
+
+*/
---
+++ SumAssignment1.java
@@ -0,0 +1,20 @@
+class SumAssignment1
+{
+	public static void main(String args[])
+	{
+		int a=10;
+		float b=20;
+		int c=a+(int)b;
+		System.out.println("Sum of two numbers:  " +c);
+		
+	}
+}
+/*
+
+Output :----
+
+Sum of two numbers:  30 
+
+
+ */
+
